eba886b6b6d24cd6ef0c88efc4e13312aa508753
Pavel <p.shahmin@gmail.com>
2024-08-20 22:10:04 +0300

    add: cotracker inference

--- inference/vit_worker.py
+++ inference/vit_worker.py
@@ -6,13 +6,35 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 import base64
+from segmentation.Segmentation import FaceSeg
+import cv2
 
+def Normalize(SegMask, LRPImage):
+    target_size = (224, 224) 
+    if LRPImage.shape[0:2] != target_size:
+        LRPImage = cv2.resize(LRPImage, target_size)
+    if SegMask.shape[0:2] != target_size:
+        SegMask = cv2.resize(SegMask, target_size)
 
-def on_request(ch, method, props, body):
-    request = json.loads(body)
-    print(f"Got {request}")
+    if LRPImage.dtype != np.uint8:
+        LRPImage = LRPImage.astype(np.uint8)
+    if SegMask.dtype != np.uint8:
+        SegMask = SegMask.astype(np.uint8)
+    
+    return SegMask, LRPImage
 
-    img = Image.open(BytesIO(base64.b64decode(request["img"])))
+def MergeSegLRP(SegMasks, LRPImage):
+    LRPImage = LRPImage[:, :, 2]
+    PIXEL_COUNT = {x:[] for x in SegMasks}
+    for i in SegMasks:
+        SegMask = SegMasks[i]
+        SegMask, LRPImage = Normalize(SegMask, LRPImage)
+        mask_lrp_classwise = cv2.bitwise_and(SegMask, LRPImage)
+        intensity_sum = np.sum(mask_lrp_classwise)
+        PIXEL_COUNT[i].append(intensity_sum)
+    return PIXEL_COUNT
+
+def photo_response(img):
     cam = model.get_cam(img)
     formatted = (cam * 255 / np.max(cam)).astype("uint8")
     lrp = Image.fromarray(formatted)
@@ -22,19 +44,43 @@ def on_request(ch, method, props, body):
     lrp_str = base64.b64encode(buffered.getvalue())
 
     response = {
-        "probability": float(model.predict(img)[1]) * 100,
-        "lrp": lrp_str.decode(),
+        "faces":[
+            {
+            "probability": float(model.predict(img)[1]) * 100,
+            "lrp": lrp_str.decode()
+            }
+        ]
     }
+    return response
+
+
+def video_response(vid):
+    with open('temp.mp4', 'wb') as vid_file:
+        vid_file.write(base64.b64decode(vid))
+
+
+
+def on_request(ch, method, props, body):
+    request = json.loads(body)
+    resp = {"error": "Invalid request"}
+    if "img" in request:
+        img = Image.open(BytesIO(base64.b64decode(request["img"])))
+        resp = photo_response(img)
+    if "vid" in request:
+        vid = request["vid"]
+        resp = video_response(vid)
+
     ch.basic_publish(
         exchange="",
         routing_key=props.reply_to,
         properties=pika.BasicProperties(correlation_id=props.correlation_id),
-        body=json.dumps(response),
+        body=json.dumps(resp),
     )
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 model = LRPViT()
+video_model = 
 
 HOST = os.environ["RABBITMQ_HOST"]
 USERNAME = os.environ["RABBITMQ_DEFAULT_USER"]
